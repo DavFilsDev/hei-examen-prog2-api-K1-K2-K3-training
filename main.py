11bf249efcea8858cd854c764d20810b7a84774c
david <miharisoadavidfils@gmail.com>
2025-07-24 17:42:46 +0300

    [ADD]: answer of the question number 3

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, status
 from starlette.responses import Response, JSONResponse
+from pydantic import BaseModel
+from typing import List
+import json
 
 
 app = FastAPI()
+
+players_db = []
+
+class Player(BaseModel):
+    Number: int
+    Name: str
 
 @app.get("/")
 def read_root():
@@ -22,3 +31,13 @@ def hello():
 def welcome(name: str):
     message = f"Welcome {name}"
     return JSONResponse(content={"message": message}, status_code=200)
+
+@app.post("/players")
+def add_players(players: List[Player]):
+    players_db.extend(players)
+    return Response(content=json.dumps([player.dict() for player in players_db]), media_type="application/json", status_code=201)
+
+# @app.post("/players", status_code=status.HTTP_201_CREATED)
+# def add_players(players: List[Player]):
+#     players_db.extend(players)
+#     return players_db
